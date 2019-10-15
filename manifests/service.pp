b5b3c64b7645f41c521f58ff5611d708ccd1c4b4
Peter Soxx <allbetsareoff29@gmail.com>
2019-10-15 16:15:48 -0400

    fixing typo - missing comma in service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,13 +2,13 @@
 #   Allows for the Apache service to restart when triggered
 class apache::service {
   service { "${apache::service_name}":
-    alias      => 'apache_service'
+    alias      => 'apache_service',
     ensure     => $apache::service_ensure,
     enable     => $apache::service_enable,
     hasrestart => true,
     audit      => 'hasrestart',
     # before => File["apache_config"],
-    
+
 
   }
 }
