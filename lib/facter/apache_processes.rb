b861769d6ceaa3d5f87e7e880317f5cd1a62ddf7
Peter Soxx <allbetsareoff29@gmail.com>
2019-11-05 13:04:48 -0500

    adding alternate way of doing unit tests

--- lib/facter/apache_processes.rb
+++ lib/facter/apache_processes.rb
@@ -1,3 +1,22 @@
+# Facter.add(:web, :type => :aggregate) do
+	# chunk(:server) do
+		# web = {:server => 'Nginx'}
+		# web
+	# end
+	# chunk(:version) do
+		# if Facter::Core::Execution.which('nginx') != nil
+			# version = Facter::Core::Execution.execute('nginx -v | 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+')
+			# web = {:version => version}
+		# end
+		# web
+	# end
+	# chunk(:processes) do
+		# processes = Facter::Core::Execution.execute('pgrep nginx | wc -l')
+		# web = {:processes => processes}
+		# web
+	# end
+# end
+
 Facter.add(:apache_processes) do
 	confine :kernel => "Linux"
 	setcode do
